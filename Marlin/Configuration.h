a54b446a92104dad2611bf629ad6b1a677757223
insomaniac49 <Inso_maniac@outlook.com>
2022-02-28 19:28:58 -0600

    Ejerk 5-15
    Won't build because ejerk was less than 10

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1004,7 +1004,7 @@
   #endif
 #endif
 
-#define DEFAULT_EJERK    5.0  // May be used by Linear Advance
+#define DEFAULT_EJERK    15 //Less than 10 results in a sanity check  // May be used by Linear Advance
 
 /**
  * Junction Deviation Factor
